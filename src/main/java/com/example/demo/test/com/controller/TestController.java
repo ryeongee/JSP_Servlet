d622d046a6a3208f45e2b2d69a5e0418a42f9d0e
ORG <47654977+ryeongee@users.noreply.github.com>
2022-08-25 15:00:04 +0900

    .

--- src/main/java/com/example/demo/test/com/controller/TestController.java
+++ src/main/java/com/example/demo/test/com/controller/TestController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/insert/insert")
-    public String indexForm() {
+    @GetMapping("/")
+    public String mainPage() {
+        return "/main/main";
+    }
+    @GetMapping("/auth/login")
+    public String login() {
         System.out.println("get 받음");
-        return "/insert/insert";
+        return "/auth/login";
     }
 }
