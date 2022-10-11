cc9d1749dc8063495cb1608a0e3bf73dced7c070
ryeongee <ryeongee24@gmail.com>
2022-10-12 00:34:38 +0900

    .

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping
 @Controller
 public class TestController {
     @GetMapping("/")
